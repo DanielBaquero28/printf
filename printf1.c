0171710cc5fc18eb6fbfcef2d829e9140a6559fd
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-29 07:51:59 +0000

    fixed some betty style

--- printf1.c
+++ printf1.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * @format_type: Identifies if format is corrected. 
- * @format: It is a character string, defines type of data. 
+ * @format_type: Identifies if format is corrected.
+ * @format: It is a character string, defines type of data.
  * @i: Index
  * @count: Number of characters printed.
  * @valist: Hold the information needed to retrieve the additional arguments.
@@ -10,47 +10,47 @@
 
 int format_type(const char *format, int i, int count, va_list valist)
 {
-  int c;
-  if (format[i + 1] == 'c')
-    {
-  c = va_arg(valist, int);
-  _putchar(c);
-  count++;
-    }
-  char *s;
-  if (format[i + 1] == 's')
-    {
-      s = va_arg(valist, char *);
-      if (s == NULL)
+	int c;
+	if (format[i + 1] == 'c')
 	{
-	s = "(null)";
+		c = va_arg(valist, int);
+		_putchar(c);
+		count++;
 	}
-      while (*s != '\0')
+	char *s;
+	if (format[i + 1] == 's')
 	{
-	  _putchar(s[i]);
-	  s++;
-	  count++;
+		s = va_arg(valist, char *);
+		if (s == NULL)
+		{
+			s = "(null)";
+		}
+		while (*s != '\0')
+		{
+			_putchar(s[i]);
+			s++;
+			count++;
+		}
 	}
-    }
-      if (format[i + 1] == '%')
+	if (format[i + 1] == '%')
 	{
-	  int p;
-	  p = format[i + 1];
-	  _putchar(p);
-	  count++;
+		int p;
+		p = format[i + 1];
+		_putchar(p);
+		count++;
 	}
-	if (format [i + 1] == 'd' || format[i + 1] == 'i')
+	if (format[i + 1] == 'd' || format[i + 1] == 'i')
 	{
-	  int d;
-	  d = va_arg(valist, int);
-	  if (!d)
-	    {
-	      count++;
-	      putchar(48);
-	    }
-	  else
-	    count = count + print_numbers(d);
-    }
+		int d;
+		d = va_arg(valist, int);
+		if (!d)
+		{
+			count++;
+			putchar(48);
+		}
+		else
+			count = count + print_numbers(d);
+	}
 }
 
 /**
