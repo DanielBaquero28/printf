a7dc7cabd55edc7edc38eeec1c3f2bd5f4688569
DanielBaquero28 <835@holbertonschool.com>
2019-07-29 18:39:59 +0000

    Added [The calculations prototypes]

--- holberton.h
+++ holberton.h
@@ -2,7 +2,23 @@
 #define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct st 
+ *
+**/
+
+typedef struct st{
+  char *s;
+  int (*f)(va_list);
+}op_t;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_numbers(int i);
+int print_char(va_list valist);
+int print_string(va_list valist);
+int print_decimal(va_list valist);
+int print_int(va_list valist);
+int (*get_func(char s))(va_list valist);
 #endif
