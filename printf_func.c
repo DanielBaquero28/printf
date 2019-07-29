e48e333cd1eb9b70e3f83ed81fea2a5f0047e8b9
DanielBaquero28 <835@holbertonschool.com>
2019-07-29 23:58:34 +0000

    Fixed [Tab indentation on the conditionals]

--- printf_func.c
+++ printf_func.c
@@ -7,20 +7,20 @@
 
 int _printf(const char *format, ...)
 {
-	va_list valist;
-	int x;
+va_list valist;
+int x;
 
-	va_start(valist, format);
+va_start(valist, format);
 
-	for (x = 0; format[x] != '\0'; x++)
-	{
-			if (format[x] == '%')
-			  {
-			    get_func(format[x + 1])(valist);
-			    x += 2;
-			  }
-		_putchar(format[x]);
-	}
-	va_end(valist);
-	return (0);
+for (x = 0; format[x] != '\0'; x++)
+{
+if (format[x] == '%')
+{
+get_func(format[x + 1])(valist);
+x += 2;
+}
+_putchar(format[x]);
+}
+va_end(valist);
+return (0);
 }
