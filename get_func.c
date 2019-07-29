62bc3bc4f215e7aa74bfec14c028a7bfcff67760
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-07-29 17:55:17 +0000

    fixed betty

--- get_func.c
+++ get_func.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * get_func - It will identify the function and will assign the character to whe  n the character with its function.
+ * get_func - It identifies the function and assigns the character to function.
  * @s: Pointer thet goes through the array.
  * Return: Returns the function which is assigned.
  **/
 int (*get_func(char s))(va_list)
 {
-	op_ ops [] = {
+	op_ ops[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{"d", print_decimal},
@@ -14,9 +14,10 @@ int (*get_func(char s))(va_list)
 		{NULL, NULL}
 	};
 	int i;
+
 	for (i = 0; ops[i].c != NULL; i++)
 	{
-		if(*ops[i].c == s)
+		if (*ops[i].c == s)
 			return (ops[i].f);
 	}
 	return (NULL);
