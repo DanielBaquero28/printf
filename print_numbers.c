55d70f459dd4e9863e9d5722e409465bc8c96b6e
DanielBaquero28 <835@holbertonschool.com>
2019-07-29 19:26:23 +0000

    Fixed [Removed unneccesary file and added conditional with negative numbers]

--- print_numbers.c
+++ print_numbers.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
 
-void print_numbers(int n)
+/**
+ * print_decimals - Prints the numbers in decimals format. 
+ * @valist: List of decimals.
+ * Return: Number of decimals.
+**/
+
+int print_decimals(va_list valist)
 {
-int i, j = 1;
+int n, i, j = 1;
+int count = 0;
+
+if (n < 0)
+{
+n = (n * -1);
+count = count + _putchar('-'); 
+}
 i = n;
 while (i > 0)
 {
@@ -12,13 +25,19 @@ j *= 10;
 j /= 10;
 while (j > 0)
 {
-putchar(((n / j) % 10) + 48);
+count = count + _putchar(((n / j) % 10) + 48);
 j /= 10;
 }
+return(count);
 }
-int main(void)
+
+/**
+ * print_int - Prints the integers
+ * @valist: List of integers.
+ * Return: Number of integers.
+**/
+
+int print_int(va_list valist)
 {
-int n = 45;
-print_numbers(n);
-return (0);
+return (print_decimals(valist));
 }
