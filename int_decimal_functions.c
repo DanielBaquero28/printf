939623636575892cefa64829cea63e09ab2f3c5d
DanielBaquero28 <835@holbertonschool.com>
2019-07-29 18:21:38 +0000

    Fixed [Created new file because betty didn't allow more than 40 lines]

---
+++ int_decimal_functions.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_decimal - Prints number in decimal
+ * @valist: List of decimals.
+ * Return: Number of decimals. (Counter)
+**/
+
+int print_d(va_list valist)
+{
+
+}
