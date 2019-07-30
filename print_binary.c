b34ffbb28c2ca2cb0ad9c66bf78dc67741dcf960
DanielBaquero28 <835@holbertonschool.com>
2019-07-30 21:39:34 +0000

    Fixed [Starting binary]

---
+++ print_binary.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_binary - Prints integers to binaries. 
+ * @valist: List of binaries.
+ * Return: Count of bytes.
+**/
+
+int print_binary(va_valist valist)
+{
+int count, number, i, array[32];
+
+i = 0;
+count = 0;
+if (number < 0)
+{
+_putchar(48);
+count++;
+return(count);
+}
+else
+{
+while (number > 0)
+{
+array[count] = number % 2;
+number = number / 2;
+count++;
+}
+for (i = count - 1; i >= 0; i--)
+{
+_putchar(array[i] + 48);
+}
+}
+return (count);
+}
