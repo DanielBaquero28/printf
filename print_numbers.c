2796fbe4b7f38429a460b2eaa7770d72748ddbc5
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-08-02 02:45:52 +0000

    [Fix] merge fail

--- print_numbers.c
+++ print_numbers.c
@@ -8,30 +8,15 @@
 
 int print_decimals(va_list valist)
 {
-<<<<<<< HEAD
 int count = 0;
 long int num, sec_count = 1, sec_num;
-=======
-	int count = 0;
-	long int num, i, j = 1;
->>>>>>> javier
 num = va_arg(valist, int);
 if (num < 0)
 {
 num = (num * -1);
 count = count + _putchar('-');
 }
-<<<<<<< HEAD
 if (num == 0)
-=======
-if ( num == 0)
-{	_putchar(48);
-	 return (1);
-}
-i = num;
-
-while (i > 0)
->>>>>>> javier
 {
 _putchar(48);
 return(1);
@@ -39,7 +24,6 @@ return(1);
 sec_num = num;
 while (sec_num > 9)
 {
-<<<<<<< HEAD
 sec_num /= 10;
 sec_count *= 10;
 }
@@ -47,10 +31,6 @@ while (sec_count > 0)
 {
 count = count + _putchar(((num / sec_count) % 10) + 48);
 sec_count /= 10;
-=======
-count = count + _putchar(((num / j) % 10) + 48);
-j /= 10;
->>>>>>> javier
 }
 return (count);
 }
